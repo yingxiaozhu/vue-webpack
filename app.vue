c5a58608595b115d796eef3c6808434708aa70e4
chenyingjie <chenyingjie7758@163.com>
2018-08-10 13:57:01 +0800

    secondCommit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>Hello {{name}}</div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                name:'Vue.js'
+            }
+        }
+    }
+</script>
+<style scoped>
+
+    div{
+        color:#f60;
+        font-size:24px;
+    }
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var ExtractTextPlugin=require('extract-text-webpack-plugin');
+var VueLoaderPlugin=require('vue-loader/lib/plugin');
 var config = {
     entry:{
         main:'./main'
@@ -7,6 +9,39 @@ var config = {
         path:path.join(__dirname,'./dist'),
         publicPath:'/dist/',
         filename:'main.js'
-    }
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                 loader:'vue-loader',
+                 options: {
+                     loaders:{
+                         css:ExtractTextPlugin.extract({
+                             use:'class-loader',
+                             fallback:'vue-style-loader'
+                        })
+                     }
+                 }
+            },
+            {
+                test:/\.js$/,
+                loader:'babel-loader',
+                exclude:/node_modules/
+            },
+            {
+                test:/\.css$/,
+                use:ExtractTextPlugin.extract({
+                    use: 'class-loader',
+                    fallback: 'style-loader'
+                })
+            }
+
+        ]
+    },
+    plugins:[
+        new ExtractTextPlugin("main.css"),
+        new VueLoaderPlugin()
+    ]
 };
 module.exports=config;
