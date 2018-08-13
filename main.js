5bfefb52b56c1815939c68b8132e312cc75bd801
chenyingjie <chenyingjie7758@163.com>
2018-08-13 15:48:19 +0800

    页面跳转问题

--- main.js
+++ main.js
@@ -7,14 +7,23 @@ Vue.use(VueRouter);
 const Routers=[
     {
         path:'/index',
+        meta:{
+            title:'首页'
+        },
         component:(resolve) => require(['./views/index.vue'],resolve)
     },
     {
         path:'/about',
+        meta:{
+            title:'关于'
+        },
         component:(resolve) => require(['./views/about.vue'],resolve)
     },
     {
         path:'/user/:id',
+        meta:{
+            title:'个人主页'
+        },
         component:(resolve) => require(['./views/user.vue'],resolve)
     },
     {
@@ -31,6 +40,18 @@ const RouterConfig={
     routes:Routers
 };
 const router = new VueRouter(RouterConfig);
+router.beforeEach((to,from,next) =>{
+    //window.document.title= to.meta.title;
+    if(window.localStorage.getItem('token')){
+        next();
+    }else{
+        next('/login');
+    }
+
+});
+router.afterEach((to,from,next) =>{
+    window.scrollTo(0,0);
+});
 
 new Vue({
     el:'#app',
--- views/index.vue
+++ views/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>首页</div>
+    <router-link to="/about">跳转about</router-link>
 </template>
 <script>
     export default{
